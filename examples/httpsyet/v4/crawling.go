0d7c6e840fe9b4f708bea2c7fca14f13f5c2cc17
GoLangsam <GoLangsam.APA@a-bc.net>
2018-06-21 18:40:20 +0200

    simplified & apply improved function naming convention

--- examples/httpsyet/v4/crawling.go
+++ examples/httpsyet/v4/crawling.go
@@ -7,18 +7,16 @@ package httpsyet
 import (
 	"fmt"
 	"net/url"
-	"sync"
 	"time"
 
-	res "github.com/GoLangsam/pipe/examples/httpsyet/v3/result"
-	"github.com/GoLangsam/pipe/examples/httpsyet/v3/sites"
+	res "github.com/GoLangsam/pipe/examples/httpsyet/v4/result"
+	"github.com/GoLangsam/pipe/examples/httpsyet/v4/sites"
 )
 
 // ---------------------------------------------------------------------------
 
 // alias types in order to leave source code unaffected
 type site = sites.Site
-type traffic = sites.Traffic
 
 // ---------------------------------------------------------------------------
 // make `result` an explicit type, and
@@ -28,7 +26,7 @@ type result = res.Result
 
 // report prints a result to Crawler.Out;
 // used by `DoneResultFunc`.
-func (c Crawler) report(r result) {
+func (c *Crawler) report(r result) {
 	if _, err := fmt.Fprintln(c.Out, r); err != nil {
 		c.Log.Printf("failed to write output '%s': %v\n", r, err)
 	}
@@ -38,9 +36,9 @@ func (c Crawler) report(r result) {
 
 // crawling represents a crawling Crawler ... busy crawling ...
 type crawling struct {
-	Crawler             // config
-	traffic             // to be crawled
-	results chan result // to be reported
+	*Crawler                   // config
+	*sites.Traffic             // the cirular network
+	results        chan result // to be reported
 }
 
 // ---------------------------------------------------------------------------
@@ -49,7 +47,7 @@ type crawling struct {
 // goWaitAndClose is to be used after initial traffic has been added.
 func (c *crawling) goWaitAndClose() {
 	go func(c *crawling) {
-		<-sites.DoneSiteWait(c.Travel, c)
+		<-c.Done // from embedded sites.Traffic
 		close(c.results)
 	}(c)
 }
@@ -57,15 +55,9 @@ func (c *crawling) goWaitAndClose() {
 // ===========================================================================
 
 // crawl performs a crawling Crawler's main function: crawl.
-//
-// This version attempts to respect the original implementation.
-// Thus, it is still too busy - catering with too many concerns;
-// concerns which might better be catered for in the traffic crawling processor's pipe network;
-// passed to traffic.processor, and used by `DoneSiteFunc`.
 func (c *crawling) crawl(s site) {
 	urls := c.crawlSite(s)         // the core crawl process
 	c.Feed(urls, s.URL, s.Depth-1) // new urls enter crawling - circular feedback
-	c.Done()                       // site leaves crawling
 	time.Sleep(c.Delay)            // have a gentle nap
 }
 
@@ -73,17 +65,16 @@ func (c *crawling) crawl(s site) {
 
 // a Crawler crawling: crawl traffic, emit results
 // and signal when done
-func (c Crawler) crawling(urls []*url.URL) (done <-chan struct{}) {
+func (c *Crawler) crawling(urls []*url.URL) (done <-chan struct{}) {
 	crawling := crawling{
-		c, // "Crawler is used as configuration ..."
-		traffic{
-			make(chan site),     // the feedback traffic
-			new(sync.WaitGroup), // monitor traffic
-		},
+		c,                 // "Crawler is used as configuration ..."
+		sites.New(),       // the cirular network
 		make(chan result), // results - the (secondary) output
 	}
+
 	crawling.crawling(urls, parallel(c.Parallel))
-	return res.DoneResultFunc(crawling.results, c.report)
+
+	return res.DoneFunc(crawling.results, c.report)
 }
 
 // ===========================================================================
