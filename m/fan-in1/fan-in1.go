1208205f4fd36e5d4eb3811f2df3213af3ce0606
GoLangsam <GoLangsam.APA@a-bc.net>
2018-06-25 08:05:16 +0200

    new battery: fan-in1

--- m/fan-in1/fan-in1.go
+++ m/fan-in1/fan-in1.go
@@ -13,6 +13,10 @@ import (
 // anyThing is the generic type flowing thru the pipe network.
 type anyThing generic.Type
 
+// anyOwner is the generic who shall own the methods.
+//  Note: Need to use `generic.Number` here as `generic.Type` is an interface and cannot have any method.
+type anyOwner generic.Number
+
 // ===========================================================================
 // Beg of anyThingFanIn1 - fan-in using only one go routine
 
