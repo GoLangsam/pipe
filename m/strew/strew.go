0feecb3a4c6e780654b0cbbc149a7b0f2dbb04ec
GoLangsam <GoLangsam.APA@a-bc.net>
2018-06-28 11:16:05 +0200

    strew: now random round-robin (via range map) & original sequential version downgraded => ss.notyet/

--- m/strew/strew.go
+++ m/strew/strew.go
@@ -23,16 +23,18 @@ type anyOwner generic.Number
 // anyThingStrew returns a slice (of size = size) of channels
 // one of which shall receive each inp before close.
 func (my anyOwner) anyThingStrew(inp <-chan anyThing, size int) (outS [](<-chan anyThing)) {
-	chaS := make([]chan anyThing, size)
+	chaS := make(map[chan anyThing]struct{}, size)
 	for i := 0; i < size; i++ {
-		chaS[i] = make(chan anyThing)
+		chaS[make(chan anyThing)] = struct{}{}
 	}
 
-	go my.strewanyThing(inp, chaS...)
+	go my.strewanyThing(inp, chaS)
 
 	outS = make([]<-chan anyThing, size)
-	for i := 0; i < size; i++ {
-		outS[i] = chaS[i] // convert `chan` to `<-chan`
+	i := 0
+	for c := range chaS {
+		outS[i] = (<-chan anyThing)(c) // convert `chan` to `<-chan`
+		i++
 	}
 
 	return outS
@@ -41,25 +43,25 @@ func (my anyOwner) anyThingStrew(inp <-chan anyThing, size int) (outS [](<-chan 
 // c strewanyThing(inp <-chan anyThing, outS ...chan<- anyThing) {
 // Note: go does not convert the passed slice `[]chan anyThing` to `[]chan<- anyThing` automatically.
 // So, we do neither here, as we are lazy (we just call an internal helper function).
-func (my anyOwner) strewanyThing(inp <-chan anyThing, outS ...chan anyThing) {
+func (my anyOwner) strewanyThing(inp <-chan anyThing, outS map[chan anyThing]struct{}) {
 
 	for i := range inp {
-		for !my.trySendanyThing(i, outS...) {
+		for !my.trySendanyThing(i, outS) {
 			time.Sleep(time.Millisecond * 10) // wait a little before retry
 		} // !sent
 	} // inp
 
 	for o := range outS {
-		close(outS[o])
+		close(o)
 	}
 }
 
-func (my anyOwner) trySendanyThing(inp anyThing, outS ...chan anyThing) bool {
+func (my anyOwner) trySendanyThing(inp anyThing, outS map[chan anyThing]struct{}) bool {
 
 	for o := range outS {
 
 		select { // try to send
-		case outS[o] <- inp:
+		case o <- inp:
 			return true
 		default:
 			// keep trying
