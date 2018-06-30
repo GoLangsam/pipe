3051d31f3d534e65dca007ee47a579fe9b5c2d50
GoLangsam <GoLangsam.APA@a-bc.net>
2018-06-30 13:53:44 +0200

    make golint more happy :-)

--- s/join/nogodoc.go
+++ s/join/nogodoc.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// package pipe provides functions
+// Package pipe provides functions
 // useful to build a network of concurrent pipe processes
 // the components of which are connected by channels.
 //
